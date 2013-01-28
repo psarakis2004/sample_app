b5df4903d5fb788dc014015bbb627d0b5b2e2eee
manos <psarakis2004@yahoo.gr>
2013-01-28 21:23:45 +0200

    Use let var in test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-
+let(:base_title) {"Ruby on Rails Tutorial Sampe App"}
 
 	describe "Home page"do
 
@@ -14,7 +14,7 @@ describe "StaticPages" do
       it "should have the title 'Home'"do
   visit '/static_pages/home'
   page.should have_selector('title',
-   :text=>'Ruby on Rails Tutorial Sample App | Home')
+   :text=>"#{base_title}| Home")
     end
   end
   
@@ -30,7 +30,7 @@ describe "StaticPages" do
   it "should have the title 'Help'"do
   visit '/static_pages/help'
   page.should have_selector('title',
-   :text=>'Ruby on Rails Tutorial Sample App | Help')
+   :text=>"#{base_title}| Help")
     end
   end
 
@@ -44,7 +44,7 @@ end
 it "should have the title 'About Us'"do
   visit '/static_pages/about'
   page.should have_selector('title',
-   :text=>'Ruby on Rails Tutorial Sample App | About Us')
+   :text=>"#{base_title}| About Us")
     end
 end
 
@@ -58,7 +58,7 @@ end
 it "should have the title 'Contact'"do
   visit '/static_pages/contact'
   page.should have_selector('title',
-   :text=>'Ruby on Rails Tutorial Sample App | Contact')
+   :text=>"#{base_title}| Contact")
     end
 end
 
